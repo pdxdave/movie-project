e609085e6ac92904cb84608c7a3d85d8427c90fd
pdxdave <webdevdave@hotmail.com>
2020-04-29 21:17:14 -0700

    homepage and hooks file

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import Banner from './components/Banner'
+
+import {useGetMovies} from './components/hooks/useGetMovies.js'
+
+import {
+    URL,
+    KEY,
+    SEARCH,
+    POPULAR_SEARCH,
+    BASE_URL,
+    BACKDROP_SIZE,
+    POSTER_SIZE } from './helpers/config.js'
 
 
 const HomePage = () => {
+
+    const [{state, loading, error}, getMovies] = useGetMovies()
+
+    console.log(state)
+
     return (
         <React.Fragment>
-        
-
+            <Banner />
         </React.Fragment>
     )
 }
